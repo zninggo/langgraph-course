f60ed4852ae54391d9d5d652af29d9d2ce7ca73f
lupinus <zninggo@outlook.com>
2026-05-06 20:23:57 +0800

    graph state 定义

---
+++ app/graph/state.py
@@ -0,0 +1,18 @@
+from typing import TypedDict, Literal, List
+
+
+class GraphState(TypedDict):
+    """
+    langgraph 中的 全局状态
+
+    属性:
+        question: 问题
+        generation: 答案
+        web_search: 是否需要网络搜索
+        documents: 文档列表
+    """
+
+    question: str
+    generation: str
+    web_search: bool
+    documents: List[str]
